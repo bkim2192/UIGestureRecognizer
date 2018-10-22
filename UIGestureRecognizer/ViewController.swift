891e63579ffaa9ceb6b1b68f08092c6d1e611562
Brandon Kim <bkim2192@jh-mba158-16.d214.org>
2018-10-22 15:08:22 -0500

    finished

--- UIGestureRecognizer/ViewController.swift
+++ UIGestureRecognizer/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var greenView: UIView!
+    @IBOutlet weak var blueView: UIView!
+    
+    var isBlue = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
 
+    @IBAction func changeBackgroundColorOnTap(_ sender: UITapGestureRecognizer) {
+        if isBlue {
+            blueView.backgroundColor = greenView.backgroundColor
+        } else {
+            blueView.backgroundColor = UIColor(red: 0 / 255.0, green: 128 / 255.0, blue: 255 / 255.0, alpha: 1)
+        }
+        isBlue = !isBlue
+    }
+    
 }
 
